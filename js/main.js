b2e6aab936735e1e1ef33db4e462195c6b3388b7
Ricardo Quesada <ricardoquesada@gmail.com>
2012-11-05 19:01:24 -0800

    Simplified JS code
    removed cocos2d code since it is part of cocos2d-iphone

---
+++ js/main.js
@@ -0,0 +1,67 @@
+/* http://www.cocosbuilder.com
+ * http://www.cocos2d-iphone.org
+ *
+ * Copyright (c) 2012 Zynga, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+//------------------------------------------------------------------
+//
+// JavaScript sample
+//
+//------------------------------------------------------------------
+
+
+// Loads cocos2d, chipmunk constants and helper functions
+require("jsb_constants.js");
+
+// Loads the game's files
+require("js/MainMenuScene.js");
+require("js/GameScene.js");
+require("js/Level.js");
+require("js/Dragon.js");
+require("js/Coin.js");
+require("js/Bomb.js");
+require("js/Explosion.js");
+require("js/EndCoin.js");
+
+//------------------------------------------------------------------
+//
+// Main entry point
+//
+//------------------------------------------------------------------
+function run()
+{
+    cc.log("run");
+
+    var director = cc.Director.getInstance();
+
+    cc.log("director: "+director);
+
+    var scene = cc.Reader.loadAsScene("MainMenuScene.ccbi");
+
+    var runningScene = director.getRunningScene();
+    if( runningScene === null )
+        director.runWithScene( scene );
+    else
+        director.replaceScene( cc.TransitionFade.create(0.5, scene ) );
+}
+
+run();
